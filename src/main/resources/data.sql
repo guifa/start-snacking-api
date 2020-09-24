9452f011ac8b08e23416461ed84637937c26b374
Luiz Guilherme Ferreira de Alcantara <luizguilherme.alcantara@unimedfortaleza.com.br>
2020-09-24 20:19:49 -0300

    Change ingredient name

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ values('Alface', 0.4);
 insert into ingredient(name,price)
 values('Bacon', 2.0);
 insert into ingredient(name,price)
-values('Hambúrguer de carne', 3.0);
+values('Carne', 3.0);
 insert into ingredient(name,price)
 values('Ovo', 0.8);
 insert into ingredient(name,price)
